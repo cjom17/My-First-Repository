c6c03c791879fcd8c029f04cd68aeadc7abd0224
cjom17 <nztechnophile.1@gmail.com>
2022-09-15 18:30:50 +0800

    added subtraction

---
+++ subtraction.java
@@ -0,0 +1,17 @@
+
+
+public static void subtraction(){
+    int nOne, nTwo;
+    Scanner kb = new Scanner(System.in);
+
+    System.out.println("Subtraction");
+
+    System.out.print("\nFirst Number: ");
+    nOne = kb.nextInt();
+
+    System.out.print("\nSecond Number: ");
+    nTwo = kb.nextInt();
+
+    kb.close();
+    System.out.println("\nSum: " + nOne + " - " + nTwo + " = " + (nOne - nTwo));
+}

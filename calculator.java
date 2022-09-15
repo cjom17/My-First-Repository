1cdb6d7812650308249f8ca759bb4cf14eff00ae
cjom17 <nztechnophile.1@gmail.com>
2022-09-15 18:38:57 +0800

    added multiplication

--- calculator.java
+++ calculator.java
@@ -1,66 +1,15 @@
-public class Calculator {
+public static void multiplication(){
+    int nOne, nTwo;
+    Scanner kb = new Scanner(System.in);
 
+    System.out.println("Multiplication");
 
-    public static void main(String[] args) {
+    System.out.print("\nFirst Number: ");
+    nOne = kb.nextInt();
 
-        Scanner kb = new Scanner(System.in);
+    System.out.print("\nSecond Number: ");
+    nTwo = kb.nextInt();
 
-        System.out.println("Simple Calculator");
-
-        System.out.println("\nHere are your options:");
-        System.out.println("\n1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Division");
-        System.out.println("4. Multiplication");
-
-        System.out.print("\nWhat would you like to do?: ");
-        int choice = kb.nextInt();
-        System.out.println();
-
-
-        if (choice == 1){
-            addition();
-        }
-        else if (choice == 2){
-            subtraction();
-        }
-        else if (choice == 3){
-            division();
-        }
-        else if (choice == 4){
-            multiplication();
-        }
-
-        System.out.println();
-        kb.close();
-    }
-
-    // For addition here >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    public static void addition(){
-
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Addition");
-
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
-    }
-
-
-    // For addition here >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-    // For addition here >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-    
-
-  
+    kb.close();
+    System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));
 }

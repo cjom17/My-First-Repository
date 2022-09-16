741b2085a4d5520ad2e78e8bf0e247584385fe18
cjom17 <nztechnophile.1@gmail.com>
2022-09-16 15:11:20 +0800

    adde

---
+++ calcu.java
@@ -0,0 +1,16 @@
+public static void multiplication(){
+    int nOne, nTwo;
+    Scanner kb = new Scanner(System.in);
+
+    System.out.println("Multiplication");
+
+    System.out.print("\nFirst Number: ");
+    nOne = kb.nextInt();
+
+    System.out.print("\nSecond Number: ");
+    nTwo = kb.nextInt();
+
+    kb.close();
+    System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));
+}
+}
